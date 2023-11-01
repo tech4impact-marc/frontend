181c027cf4bd34cc841237c376869aafb073ec2f
bear_ddozan <sgh00010119@kaist.ac.kr>
2023-11-01 20:28:49 +0900

    docs: merge 전 index파일 수정
    index.tsx에 있던 내용을 reportshare.tsx로 옮기고, index.tsx는 메인브랜치와 동일하게 설정

--- components/Layout/BottomNav.tsx
+++ components/Layout/BottomNav.tsx
@@ -20,7 +20,7 @@ const CustomPaper = styled(Paper)`
 export default function BottomNav() {
   const navLinks = [
     { name: 'Map', icon: <LocationOn />, path: '/' },
-    { name: '제보하기', icon: <FmdBad />, path: '/' },
+    { name: '제보하기', icon: <FmdBad />, path: '/reportshare' },
     { name: '마이페이지', icon: <Person />, path: '/' },
   ]
 
