4ff27b0c7d372e35b3ecfc627f8a953c9e9f80cb
presenthee <khyunhii@gmail.com>
2023-11-17 00:44:09 +0900

    style: 바텀 네비게이션 색상 수정

--- components/Layout/BottomNav.tsx
+++ components/Layout/BottomNav.tsx
@@ -7,7 +7,12 @@ import NextLink from 'next/link'
 import { SyntheticEvent, useState } from 'react'
 
 const CustomBottomNavAction = (props: any) => {
-  return <BottomNavigationAction sx={{ maxWidth: '30%' }} {...props} />
+  return (
+    <BottomNavigationAction
+      sx={{ maxWidth: '30%', '&.Mui-selected': { color: '#000' }, color: '#bec1c7' }}
+      {...props}
+    />
+  )
 }
 
 const CustomPaper = styled(Paper)`
@@ -19,9 +24,9 @@ const CustomPaper = styled(Paper)`
 `
 export default function BottomNav() {
   const navLinks = [
-    { name: '지도', icon: <MapOutlined sx={{ color: '#bec1c7' }} />, path: '/map' },
-    { name: '제보하기', icon: <AddOutlined sx={{ color: '#bec1c7' }} />, path: '/' },
-    { name: '마이페이지', icon: <AccountCircleRounded sx={{ color: '#bec1c7' }} />, path: '/' },
+    { name: '지도', icon: <MapOutlined />, path: '/map' },
+    { name: '제보하기', icon: <AddOutlined />, path: '/' },
+    { name: '마이페이지', icon: <AccountCircleRounded />, path: '/' },
   ]
 
   const [value, setValue] = useState(1)
