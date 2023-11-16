8fceb9865683b5289bd4cb5c05f0ddd4e8618ba2
presenthee <khyunhii@gmail.com>
2023-11-16 19:53:23 +0900

    style: 네비게이션 아이콘 및 색상 변경

--- components/Layout/BottomNav.tsx
+++ components/Layout/BottomNav.tsx
@@ -1,4 +1,4 @@
-import { FmdBad, LocationOn, Person } from '@mui/icons-material'
+import { AccountCircleRounded, AddOutlined, MapOutlined } from '@mui/icons-material'
 import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import Paper from '@mui/material/Paper'
@@ -12,16 +12,16 @@ const CustomBottomNavAction = (props: any) => {
 
 const CustomPaper = styled(Paper)`
   position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
   width: 100%;
 `
 export default function BottomNav() {
   const navLinks = [
-    { name: '지도', icon: <LocationOn />, path: '/map' },
-    { name: '제보하기', icon: <FmdBad />, path: '/' },
-    { name: '마이페이지', icon: <Person />, path: '/' },
+    { name: '지도', icon: <MapOutlined sx={{ color: '#bec1c7' }} />, path: '/map' },
+    { name: '제보하기', icon: <AddOutlined sx={{ color: '#bec1c7' }} />, path: '/' },
+    { name: '마이페이지', icon: <AccountCircleRounded sx={{ color: '#bec1c7' }} />, path: '/' },
   ]
 
   const [value, setValue] = useState(1)
