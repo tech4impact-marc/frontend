e935cce13adbb9403f1f77596850e3c37c3a9b9b
bear_ddozan <sgh00010119@kaist.ac.kr>
2023-11-24 02:41:49 +0900

    fix: 로그아웃 처리 수정
    API 변화로 인한 로그아웃 처리 방식 수정

--- pages/mypage/user/setting.tsx
+++ pages/mypage/user/setting.tsx
@@ -14,42 +14,8 @@ export default function SettingPage() {
     router.back()
   }
 
-  const handleLogOut = async () => {
-    const isLogOut = window.confirm('로그아웃 하시겠습니까?')
-    if (isLogOut) {
-      try {
-        const jwtToken = await refreshAccessToken()
-        console.log(jwtToken)
-        const response = await axios.post(
-          `${process.env.NEXT_PUBLIC_IP_ADDRESS}/auth/logout`,
-          null,
-          {
-            headers: {
-              Authorization: `Bearer ${jwtToken.accessToken}`,
-            },
-            withCredentials: true,
-          }
-        )
-        if (response.status !== 200) {
-          throw new Error('Network response was not ok')
-        }
-        // 로그아웃 성공 시 처리
-
-        store.dispatch({ type: 'SET_TOKENS', payload: {} })
-        store.dispatch({ type: 'SET_USER', payload: {} })
-        store.dispatch({ type: 'SET_ACCESSTOKEN_EXPIRESAT', payload: 0 })
-        store.dispatch({ type: 'SET_REFRESHTOKEN_EXPIRESAT', payload: 0 })
-
-        console.log('로그아웃 성공', response)
-        alert('로그아웃 되었습니다.')
-        router.push('/')
-      } catch (error) {
-        // 오류 처리
-        console.error('로그아웃 중 오류가 발생했습니다.', error)
-      }
-    } else {
-      alert('로그아웃 취소')
-    }
+  const handleLogOut = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_IP_ADDRESS}/auth/kakao/logout`
   }
   const handleUnregister = async () => {
     const isUnregister = window.confirm('회원탈퇴 하시겠습니까?')
@@ -93,10 +59,6 @@ export default function SettingPage() {
     }
   }
 
-  const handleLogOut2 = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_IP_ADDRESS}/auth/kakao/logout`
-  }
-
   return (
     <Container
       sx={{
@@ -135,13 +97,6 @@ export default function SettingPage() {
           sx={{ width: '100%', color: 'grey', justifyContent: 'flex-start' }}
         >
           회원탈퇴
-        </Button>
-        <Divider sx={{ width: '100%' }} />
-        <Button
-          onClick={handleLogOut2}
-          sx={{ width: '100%', color: 'grey', justifyContent: 'flex-start' }}
-        >
-          로그아웃2
         </Button>
         <Divider sx={{ width: '100%' }} />
       </Container>
