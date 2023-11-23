677071855e8072bcd5e2d8805551cc71c2706993
Hyun A Kim <hyuna@Violet.local>
2023-11-23 22:43:56 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google'
 import { ReactElement } from 'react'
 
+import CommonLayout from '@/components/Layout/CommonLayout'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
