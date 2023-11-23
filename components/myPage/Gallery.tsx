d3f9900257d5b5c9d433adc37cd9a04c93fc7115
presenthee <khyunhii@gmail.com>
2023-11-23 21:40:15 +0900

    style: 스타일링 포맷 수정

--- components/myPage/Gallery.tsx
+++ components/myPage/Gallery.tsx
@@ -13,8 +13,14 @@ export default function Gallery({ images }: GalleryProps) {
       <Grid container rowSpacing={1} columnSpacing={1}>
         {images.map((image, index) => (
           <Grid xs={4} md={4} key={index} justifyContent={'center'} display={'flex'}>
-            <Box width={'100%'} height={'auto'} sx={{ aspectRatio: 1 }} position="relative">
-              <Image src={image} alt={'grid image'} layout="fill" />
+            <Box
+              width={'100%'}
+              height={'auto'}
+              sx={{ aspectRatio: 1, borderRadius: '0.5rem' }}
+              position="relative"
+              overflow={'hidden'}
+            >
+              <Image src={image} alt={'grid image'} fill />
             </Box>
           </Grid>
         ))}
--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,3 +1,4 @@
+import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined'
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
 import Avatar from '@mui/material/Avatar'
 import Box from '@mui/material/Box'
@@ -8,65 +9,71 @@ import Typography from '@mui/material/Typography'
 import { ReactElement } from 'react'
 
 import CommonLayout from '@/components/Layout/CommonLayout'
-import Gallery from '@/components/myPage/Gallery'
+import Gallery from '@/components/MyPage/Gallery'
 
 interface MyPageProps {}
 
 const MyPageContainer = styled(Container)`
   display: flex;
   flex-direction: column;
-  padding: 1rem;
 `
 
-const InfoContainer = styled(Container)`
+const SectionContainer = styled(Container)`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
-  margin-bottom: 1rem;
+  padding: 1rem;
+`
+
+const InfoBox = styled(Box)`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
 `
 
 const InlineTypography = styled(Typography)`
   display: inline-block;
   margin-right: 0.5rem;
 `
-
 export default function MyPage() {
   return (
-    <MyPageContainer>
-      <InfoContainer disableGutters>
-        <Box display={'flex'}>
-          <Avatar alt="미남강현" src="/test.jpeg" sx={{ width: '4.5rem', height: '4.5rem' }} />
-          <IconButton>편집</IconButton>
-          <IconButton>
-            <SettingsOutlinedIcon />
-          </IconButton>
+    <MyPageContainer disableGutters>
+      <SectionContainer>
+        <Box display={'flex'} justifyContent={'space-between'}>
+          <Avatar alt="프로필사진" src="/test.jpeg" sx={{ width: '4.5rem', height: '4.5rem' }} />
+          <Box>
+            <IconButton sx={{ color: 'black' }}>
+              <EmojiEventsOutlinedIcon />
+            </IconButton>
+            <IconButton sx={{ color: 'black' }}>
+              <SettingsOutlinedIcon />
+            </IconButton>
+          </Box>
         </Box>
-        <Box>
-          <InlineTypography fontSize={'18px'} fontWeight={700}>
-            미남강현
-          </InlineTypography>
-          <InlineTypography fontSize={'16px'} fontWeight={600}>
-            초보 탐험가
-          </InlineTypography>
-        </Box>
-        <Typography fontSize={'13px'} color={'#8f8f8f'} fontWeight={600}>
+        <InfoBox gap={'1rem'} alignItems={'flex-start'}>
+          <Typography variant="h2">미남강현</Typography>
+          <Typography variant="h4">초보 탐험가</Typography>
+        </InfoBox>
+        <Typography variant="h5" color={'#8f8f8f'} fontWeight={600}>
           리포트 5
         </Typography>
-        <Box>
-          <InlineTypography fontSize={'13px'} color={'#8f8f8f'} fontWeight={600}>
+        <InfoBox>
+          <Typography variant="h5" color={'#8f8f8f'} fontWeight={600}>
             남방큰돌고래 5
-          </InlineTypography>
-          <InlineTypography fontSize={'13px'}>|</InlineTypography>
-          <InlineTypography fontSize={'13px'} color={'#8f8f8f'} fontWeight={600}>
+          </Typography>
+          <Typography variant="body2">|</Typography>
+          <Typography variant="h5" color={'#8f8f8f'} fontWeight={600}>
             바다거북 2
-          </InlineTypography>
-          <InlineTypography fontSize={'13px'}>|</InlineTypography>
-          <InlineTypography fontSize={'13px'} color={'#8f8f8f'} fontWeight={600}>
+          </Typography>
+          <Typography variant="body2">|</Typography>
+          <Typography variant="h5" color={'#8f8f8f'} fontWeight={600}>
             상괭이 0
-          </InlineTypography>
-        </Box>
-      </InfoContainer>
-      <Gallery images={['/test.jpeg', '/test.jpeg', '/test.jpeg', '/test.jpeg']} post_id={1} />
+          </Typography>
+        </InfoBox>
+      </SectionContainer>
+      <SectionContainer>
+        <Gallery images={['/test.jpeg', '/test.jpeg', '/test.jpeg', '/test.jpeg']} post_id={1} />
+      </SectionContainer>
     </MyPageContainer>
   )
 }
