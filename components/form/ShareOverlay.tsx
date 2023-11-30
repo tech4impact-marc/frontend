5603bedbe73f447161f00d009ecc68bee95aed65
bear_ddozan <sgh00010119@kaist.ac.kr>
2023-11-30 23:08:29 +0900

    fix: 공유 컴포넌트 props 추가
    공유 기능이 두 곳(리포트끝, 포스트)에서 가능하기 때문에 장소에 따라 피드의 제목과 내용을 수정했습니다.
    - 추가된 props
    title<string> = 공유된 피드의 제목
    value<string> = 공유된 피드의 내용

--- components/form/ShareOverlay.tsx
+++ components/form/ShareOverlay.tsx
@@ -98,6 +98,8 @@ const ShareOverlay = ({ animal, imgSrc }: { animal: string | undefined; imgSrc?:
           isOpen={isSNSShareVisible}
           onClose={handleShareClose}
           imageUrl={imgSrc}
+          title="제가 찾은 해양 생물이에요!"
+          value="저희와 함께 해양 생물을 찾아보아요!"
         />
       )}
     </React.Fragment>
