e5b7f32a0092c9782dc84189adbab077787495be
presenthee <khyunhii@gmail.com>
2023-12-01 09:34:14 +0900

    fix: 더미 url config에 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['drive.google.com'],
+    domains: ['drive.google.com', 'multerimagelocationurl1.com'],
     remotePatterns: [
       {
         protocol: 'https',
